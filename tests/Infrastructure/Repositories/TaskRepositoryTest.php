16b79ea89d8b0aa0a222d651df04ec8d8172105b
Tiago Ramos <tiagoramos1082@gmail.com>
2026-08-30 22:25:45 -0300

    feat: add DeleteById Usercase and tests

--- tests/Infrastructure/Repositories/TaskRepositoryTest.php
+++ tests/Infrastructure/Repositories/TaskRepositoryTest.php
@@ -40,6 +40,31 @@ class TaskRepositoryTest extends TestCase
             'id'=>$task->getId()
         ]);
     }
+
+    public function testMustDeleteATaskWithId(): void
+    {
+        $connection = Connection::create();
+        $repository = new TaskRepository($connection);
+
+        $task = new Task('Arrumar Quarto', "Quarto do Vitor", "2026-08-23 15:43:55");
+
+        $repository->save($task);
+
+        $repository->deleteById($task->getId());
+
+        $sql = '
+            select id from tasks where id = :id;
+        ';
+
+        $stmt = $connection->prepare($sql);
+        $stmt->execute([
+            'id'=>$task->getId()
+        ]);
+
+        $resultId = $stmt->fetchColumn();
+
+        $this->assertSame(false, $resultId);
+    }
 }
 
 

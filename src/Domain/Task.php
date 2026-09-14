1530b18f552322d7c92b656434333888e19293be
tiagoramos1082 <tiagoramos1082@gmail.com>
2026-09-13 22:34:43 -0300

    fix: change function set Description to public

--- src/Domain/Task.php
+++ src/Domain/Task.php
@@ -88,6 +88,13 @@ class Task
         return  $this->description;
     }
 
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+
+
     public function getStatus(): Status
     {
         return $this->status;

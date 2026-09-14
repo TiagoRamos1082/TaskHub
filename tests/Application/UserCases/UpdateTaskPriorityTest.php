221f2a01fc1350d1b10c5b9c4a5aa7154f6d7273
tiagoramos1082 <tiagoramos1082@gmail.com>
2026-09-13 22:31:11 -0300

    feat: add update Task Priority and tests

---
+++ tests/Application/UserCases/UpdateTaskPriorityTest.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+
+namespace Tests\Application\UserCases;
+
+use App\Application\UserCases\UpdateTaskPriority;
+use App\Domain\Task;
+use App\Infrastructure\Repositories\TaskRepository;
+use PHPUnit\Framework\TestCase;
+
+class UpdateTaskPriorityTest extends TestCase
+{
+    public function testMustUpdateTaskName(): void
+    {
+        $repository = $this->createMock(TaskRepository::class);
+
+        $repository
+            ->expects($this->once())
+            ->method('put');
+
+        $task = new Task('Arrumar Quarto', "Quarto do Vitor", "2026-08-23 15:43:55");
+
+        $updateTask = new UpdateTaskPriority($repository);
+
+        $result = $updateTask->execute(2, $task);
+
+        $this->assertSame(true, $result);
+    }
+}
+
+?>
